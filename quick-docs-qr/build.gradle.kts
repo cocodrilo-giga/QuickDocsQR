138b0bbfe85053644e03b7b13912ea7ff9ce3b18
shcherbakov.e <shcherbakov.e@7gis.ru>
2025-01-21 23:55:19 +0500

    Победил AGP

--- quick-docs-qr/build.gradle.kts
+++ quick-docs-qr/build.gradle.kts
@@ -1,7 +1,6 @@
-// /build.gradle.kts
 plugins {
-    id("com.android.application") version "8.1.4" apply false
-    id("com.android.library") version "8.1.4" apply false
+    id("com.android.application") version "8.7.3" apply false
+    id("com.android.library") version "8.7.3" apply false
     id("org.jetbrains.kotlin.android") version "1.9.24" apply false
     id("org.jetbrains.kotlin.kapt") version "1.9.24" apply false
     id("com.google.dagger.hilt.android") version "2.48" apply false
