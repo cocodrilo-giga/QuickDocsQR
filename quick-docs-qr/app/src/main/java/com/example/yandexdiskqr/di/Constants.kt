ec8e78610ccb7b057d7ee660d6ef4668758281f7
shcherbakov.e <shcherbakov.e@7gis.ru>
2025-01-23 22:14:15 +0500

    Загружаются папки!

--- quick-docs-qr/app/src/main/java/com/example/yandexdiskqr/di/Constants.kt
+++ quick-docs-qr/app/src/main/java/com/example/yandexdiskqr/di/Constants.kt
@@ -1,9 +1,9 @@
 package com.example.yandexdiskqr.di
 
 object Constants {
-    const val CLIENT_SECRET = "98e3d384277b43f5961a03293b55aab5"
+    const val CLIENT_SECRET = "78ee0609d84e4ccf944bf6bbb5afb185"
     // Замените на реальный CLIENT_ID из консоли разработчика Яндекс
-    const val CLIENT_ID = "2bd1d08ce76e44d8ab1a0077fe7b1db2"
+    const val CLIENT_ID = "3c8adfd6e64a47089f6a2e0c262c888a"
     const val REDIRECT_URI = "ydiskqr://auth"
     const val OAUTH_URL = "https://oauth.yandex.ru/authorize"
     const val TOKEN_URL = "https://oauth.yandex.ru/token"
