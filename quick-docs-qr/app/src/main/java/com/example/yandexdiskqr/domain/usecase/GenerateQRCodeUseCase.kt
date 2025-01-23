61b21a708369af3b9347715ea7461bb2f8099e93
shcherbakov.e <shcherbakov.e@7gis.ru>
2025-01-23 22:46:06 +0500

    Работает!

--- quick-docs-qr/app/src/main/java/com/example/yandexdiskqr/domain/usecase/GenerateQRCodeUseCase.kt
+++ quick-docs-qr/app/src/main/java/com/example/yandexdiskqr/domain/usecase/GenerateQRCodeUseCase.kt
@@ -1,15 +1,24 @@
-// ./src/main/java/com/example/yandexdiskqr/domain/usecase/GenerateQRCodeUseCase.kt
 package com.example.yandexdiskqr.domain.usecase
 
 import android.graphics.Bitmap
 import com.google.zxing.BarcodeFormat
+import com.google.zxing.EncodeHintType
 import com.google.zxing.qrcode.QRCodeWriter
+import java.nio.charset.Charset
 import javax.inject.Inject
 
 class GenerateQRCodeUseCase @Inject constructor() {
     operator fun invoke(folderPath: String, width: Int = 512, height: Int = 512): Result<Bitmap> = runCatching {
         val writer = QRCodeWriter()
-        val bitMatrix = writer.encode(folderPath, BarcodeFormat.QR_CODE, width, height)
+
+        // Определение хинтов для кодировки
+        val hints = mapOf(
+            EncodeHintType.CHARACTER_SET to Charset.forName("UTF-8").name(),
+            EncodeHintType.ERROR_CORRECTION to com.google.zxing.qrcode.decoder.ErrorCorrectionLevel.H
+        )
+
+        // Генерация битовой матрицы с хинтами
+        val bitMatrix = writer.encode(folderPath, BarcodeFormat.QR_CODE, width, height, hints)
         val bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.RGB_565)
 
         for (x in 0 until width) {
